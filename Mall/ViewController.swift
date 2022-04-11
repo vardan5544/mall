d58e4e8309debf2db4999cb7b3bea7b304767e92
Vardan <vardan5544@gmail.com>
2022-04-12 02:49:21 +0400

    Initial Commit

---
+++ Mall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mall
+//
+//  Created by Hakobjanyan Vardan on 12.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
