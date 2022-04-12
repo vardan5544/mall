1e64e429ebbdc043e11d0aaedaa1247600c891c2
Vardan <vardan5544@gmail.com>
2022-04-12 11:14:04 +0400

    new changes

--- Mall/ViewController.swift
+++ Mall/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("New Project")
+        print("new change")
     }
 
 
